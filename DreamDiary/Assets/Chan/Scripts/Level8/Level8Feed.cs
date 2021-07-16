203bb89308fc9fe59a907c424c5f0168fd525cd2
chanmob <sellingout@naver.com>
2021-07-16 23:39:05 +0900

    씬 수정

--- DreamDiary/Assets/Chan/Scripts/Level8/Level8Feed.cs
+++ DreamDiary/Assets/Chan/Scripts/Level8/Level8Feed.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Level8Feed : MonoBehaviour
 {
-    public GameObject fish;
+    public GameObject[] fish;
 
     public void AnimationAction()
     {
         Level8Manager.instance.EatSound();
-        fish.SetActive(true);
+        for(int i = 0; i < fish.Length; i++)
+        {
+            fish[i].SetActive(true);
+        }
     }
 }
